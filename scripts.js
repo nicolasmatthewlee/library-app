1a3e61930922be3c8829edb7e9c2771603037bd6
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2022-10-27 15:38:13 -0700

    added my_library update upon add book and remove book and restructured book display logic

--- scripts.js
+++ scripts.js
@@ -9,49 +9,74 @@ function Book(title,author,pages,read) {
 
 let library_grid = document.querySelector('.library-grid-container');
 
+function display_library() {
+
+    console.log(my_library);
+
+    // clear grid
+    while (library_grid.lastChild) {
+        library_grid.lastChild.remove();
+    }
+
+    // display
+    for (let i=0;i<my_library.length;i++) {
+
+        let book=my_library[i];
+
+        // create html object
+        let book_item = document.createElement('div');
+        book_item.setAttribute('class','book');
+        book_item.setAttribute('data-index',i);
+        library_grid.appendChild(book_item);
+
+        let book_title = document.createElement('div');
+        book_title.setAttribute('class','book-title');
+        book_title.textContent=book.title;
+        book_item.appendChild(book_title);
+
+        let book_author = document.createElement('div');
+        book_author.setAttribute('class','book-author');
+        book_author.textContent=book.author;
+        book_item.appendChild(book_author);
+
+        let book_pages = document.createElement('div');
+        book_pages.setAttribute('class','book-pages');
+        book_pages.textContent=book.pages;
+        book_item.append(book_pages);
+
+        let pages_label = document.createElement('div');
+        pages_label.setAttribute('class','pages-label');
+        pages_label.textContent='pages';
+        book_item.append(pages_label);
+
+        let book_read = document.createElement('button');
+        book_read.setAttribute('class','book-read');
+        book_read.textContent=book.read ? 'Read ✅' : 'Not Read ❌';
+        book_item.append(book_read);
+
+        let remove_book = document.createElement('button');
+        remove_book.setAttribute('class','book-remove');
+        remove_book.textContent='Remove';
+        remove_book.addEventListener('click',() => {
+            my_library.splice(book_item.i,1);
+            display_library();
+        });
+        book_item.append(remove_book);
+
+    }
+
+};
+
 function add_to_library() {
 
     // create new book object and add to my_library
     let book = new Book('The Cat in the Hat','Dr. Seuss',61,true);
     my_library.push(book);
 
-    // create new html object
-    let book_item = document.createElement('div');
-    book_item.setAttribute('class','book');
-    library_grid.appendChild(book_item);
-
-    let book_title = document.createElement('div');
-    book_title.setAttribute('class','book-title');
-    book_title.textContent=book.title;
-    book_item.appendChild(book_title);
-
-    let book_author = document.createElement('div');
-    book_author.setAttribute('class','book-author');
-    book_author.textContent=book.author;
-    book_item.appendChild(book_author);
-
-    let book_pages = document.createElement('div');
-    book_pages.setAttribute('class','book-pages');
-    book_pages.textContent=book.pages;
-    book_item.append(book_pages);
-
-    let pages_label = document.createElement('div');
-    pages_label.setAttribute('class','pages-label');
-    pages_label.textContent='pages';
-    book_item.append(pages_label);
-
-    let book_read = document.createElement('button');
-    book_read.setAttribute('class','book-read');
-    book_read.textContent=book.read ? 'Read ✅' : 'Not Read ❌';
-    book_item.append(book_read);
-
-    let remove_book = document.createElement('button');
-    remove_book.setAttribute('class','book-remove');
-    remove_book.textContent='Remove';
-    book_item.append(remove_book)
+    // display
+    display_library();
 
 };
-
 
 let add_button = document.querySelector('.add');
 add_button.addEventListener('click',add_to_library);
